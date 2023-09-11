e593e4f724c5620ca1f19f5718e062e744cd634f
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-09-11 15:17:13 -0300

    add new features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,28 @@
-import CommonQuestions from "../components/templates/CommonQuestions"
-import Depoiments from "../components/templates/Depoiments"
-import Depoiments2 from "../components/templates/Depoiments2"
 import Header from "../components/templates/Header"
 import Main from "../components/templates/Main"
-import Sale from "../components/templates/Sale"
-import Section1 from "../components/templates/Section1"
-import Section10 from "../components/templates/Section10"
-import Section11 from "../components/templates/Section11"
-import Section2 from "../components/templates/Section2"
-import Section3 from "../components/templates/Section3"
-import Section4 from "../components/templates/Section4"
-import Section5 from "../components/templates/Section5"
-import Section6 from "../components/templates/Section6"
-import Section7 from "../components/templates/Section7"
-import Section8 from "../components/templates/Section8"
-import Section9 from "../components/templates/Section9"
-import Footer from "../components/templates/Footer"
+// import CommonQuestions from "../components/templates/CommonQuestions"
+// import Depoiments from "../components/templates/Depoiments"
+// import Depoiments2 from "../components/templates/Depoiments2"
+// import Sale from "../components/templates/Sale"
+// import Section1 from "../components/templates/Section1"
+// import Section10 from "../components/templates/Section10"
+// import Section11 from "../components/templates/Section11"
+// import Section2 from "../components/templates/Section2"
+// import Section3 from "../components/templates/Section3"
+// import Section4 from "../components/templates/Section4"
+// import Section5 from "../components/templates/Section5"
+// import Section6 from "../components/templates/Section6"
+// import Section7 from "../components/templates/Section7"
+// import Section8 from "../components/templates/Section8"
+// import Section9 from "../components/templates/Section9"
+// import Footer from "../components/templates/Footer"
 
 const Home = ()=> { 
     return(
         <div className="w-screen">
             <Header />
             <Main />
-            <Section1 />
+            {/* <Section1 />
             <Section2 />
             <Depoiments />
             <Section3 />
@@ -37,7 +37,7 @@ const Home = ()=> {
             <Sale />
             <Section11 />
             <CommonQuestions />
-            <Footer />
+            <Footer /> */}
         </div>
     )
 }
