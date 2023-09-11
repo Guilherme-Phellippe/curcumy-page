66c4be39d73e09b6beb685c95a037e89cb382f09
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-09-11 15:29:32 -0300

    add loading on home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Vsl from "./pages/Vsl"
+import { Suspense } from "react"
 
 
 function App() {
   return (
     <BrowserRouter>
        <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Suspense fallback={<span className="text-xl text-orange-950">CARREGANDO...</span>}><Home /></Suspense>} />
           <Route path="/vsl" element={<Vsl />} />
        </Routes>
     </BrowserRouter>
