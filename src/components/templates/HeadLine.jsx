c0f08d5b54a5f6b2628a538f00f94069b82d41d5
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-09-15 21:52:05 -0300

    add new features vsl

--- src/components/templates/HeadLine.jsx
+++ src/components/templates/HeadLine.jsx
@@ -4,7 +4,7 @@ const HeadLine = () => {
     return (
         <div className="w-[95%] md:w-1/2 mx-auto my-4">
             <OptimizedImg
-                imgs={"https://i.ibb.co/1Q83jgx/Inserir-um-t-tulo-removebg-preview.webp"}
+                imgs={"https://i.ibb.co/BsgqJf6/Como-um-acidente-de-carro-me-ajudou-a-curar-a-dor-de-mais-de-20-mil-pessoas-Photo-Room-png-Photo-Roo.png"}
                 alt="Imagem do titulo"
             />
         </div>
